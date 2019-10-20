a5e8ecca1c097be4a8bf91c95d6f2fe8e31a7ede
Pedro Arnoldo Machado Duran <pedroarm@amdocs.com>
2019-10-20 11:34:32 -0500

    first commit

---
+++ src/main/java/com/expedia/test/expedia_demo/App.java
@@ -0,0 +1,13 @@
+package com.expedia.test.expedia_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
